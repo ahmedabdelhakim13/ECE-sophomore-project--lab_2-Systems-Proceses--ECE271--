145d2856a6c537d5f18b35e6d470333663e4139e
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-12 15:30:41 +0200

    j

--- task3.m
+++ task3.m
@@ -1,1 +1,15 @@
 clc
+t1 = linspace(0,100,500);
+x1=sin(2*pi*0.5*t);
+x2=sin(2*pi*2*t);
+x3=sin(2*pi*3*t);
+x4=sin(2*pi*4.5*t);
+%plotting the signals 
+figure, plot(t,x1);
+figure,plot(t,x2);
+figure, plot(t,x3);
+figure,plot(t,x4);
+
+t2= linspace(0,100,5000);
+y=sin(2*pi*2*t2);
+figure,plot(t2,y);
