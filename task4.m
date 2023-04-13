222324a7edb7baa8cb410fd4695a81d836493739
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-13 14:16:35 +0200

    third change

--- task4.m
+++ task4.m
@@ -4,10 +4,27 @@ x=cos(2*n./3);
 y=cos(8*pi*n./38);
 subplot(2,2,1);
 stem(n,x,'k-o');
+xlabel('time');
+ylabel('frequency');
+
 subplot(2,2,3);
 stem(n,y,'b-o');
+xlabel('time');
+ylabel('frequency');
+axis tight
 
 subplot(2,2,2);
 plot(n,x,'k-o');
+xlabel('time');
+ylabel('frequency');
+axis tight
+
 subplot(2,2,4);
 plot(n,y,'b-o');
+xlabel('time');
+ylabel('frequency');
+axis tight
+
+figure,stem(n,x,'r-o');
+hold on;
+stem(n,y,'b-x');
