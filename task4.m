e059918f9e395d14d681a7eea1e2d8ee392ac534
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-13 14:05:23 +0200

    second change

--- task4.m
+++ task4.m
@@ -2,7 +2,12 @@ clear
 n=linspace(0,40,20);
 x=cos(2*n./3);
 y=cos(8*pi*n./38);
-subplot(2,1,1);
+subplot(2,2,1);
 stem(n,x,'k-o');
-subplot(2,1,2);
-stem(n,y,'g-o');
+subplot(2,2,3);
+stem(n,y,'b-o');
+
+subplot(2,2,2);
+plot(n,x,'k-o');
+subplot(2,2,4);
+plot(n,y,'b-o');
