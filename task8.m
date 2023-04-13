a08f2c0ab88d3133a23400adf8c0063ee61d3650
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-13 15:47:50 +0200

    task8

---
+++ task8.m
@@ -0,0 +1,23 @@
+n=-10:10;
+real_part= exp(-0.1).*n;
+imaginary_part=exp(0.3*i).*n;
+signal= real_part .* imaginary_part;
+
+subplot(2,2,1);
+plot(n,abs(signal));
+
+
+
+subplot(2,2,2);
+plot(n,angle(signal));
+
+
+
+
+subplot(2,2,3);
+plot(n,real_part);
+
+
+
+subplot(2,2,4);
+plot(imaginary_part);
