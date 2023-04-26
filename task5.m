9965243ab13ff538c9e0f0f7e3489e1bb2f6a080
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-26 10:16:57 +0200

    Update task5.m

--- task5.m
+++ task5.m
@@ -1,34 +1,7 @@
 clc
 
-X=[1,-2,4,6,-5,8,10];  %define the sequence
+x=[0,0,0,1,-2,4,6,-5,8,10,0,0,0];  %define the sequence
+n=[0,0,0,-4,-3,-2,-1,0,1,2,0,0,0];
+stem(n,x);
 
 
-% y1 = 3x[n+2] + x[n-4] + 2x[n]
-y1 = zeros(1, length(x));
-for n = 1:length(x)
-    if n+2 <= length(x)
-        y1(n) = 3*x(n+2);
-    end
-    if n-4 >= 1
-        y1(n) = y1(n) + x(n-4);
-    end
-    y1(n) = y1(n) + 2*x(n);
-end
-
-% y2 = x[n+4]*x[n-1] + x[2-n]*x[n]
-y2 = zeros(1, length(x));
-for n = 1:length(x)
-    if n+4 <= length(x)
-        y2(n) = x(n+4)*x(n-1);
-    end
-    if 2-n >= 1 && 2-n <= length(x)
-        y2(n) = y2(n) + x(2-n)*x(n);
-    end
-    
-    % y3 = sum(x[n-k])
-y3 = zeros(1, length(x));
-for n = 1:length(x)
-    for k = 1:n
-        y3(n) = y3(n) + x(n-k+1);
-    end
-end
