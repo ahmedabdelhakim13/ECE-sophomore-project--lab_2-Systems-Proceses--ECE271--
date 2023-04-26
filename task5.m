1e5872c7353f177d47af5e3b884c4343ce9bcc69
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-26 23:13:42 +0200

    cc

--- task5.m
+++ task5.m
@@ -1,27 +1,26 @@
 clc
 
-x=[1,-2,4,6,-5,8,10];  %define the sequence
-n=linspace(-4,2,7);
+x=[0,0,0,0,1,-2,4,6,-5,8,10,0,0,0,0];  %define the sequence
+n=linspace(-8,6,15);
 
 x11=3*[ x(3:end) zeros(1,2)];
 x12=[zeros(1,4) x(1:end-4)];
 x13=2*x;
-
+figure;
 y1=x11+x12+x13;
-subplot(3,1,1)
 stem(n,y1);
 
 
 x21=[x(5:end) zeros(1,4)];      %x(n+4) shiftingleft by 4 units 
 x22= [zeros(1,1) x(1:end-1)];   %x(n-1) shifting right by one unit
-x__=  [x(3:end) zeros(1,2)];  %x(2-n)shifthing right by 2unit then reflect
-x23= [ zeros(1,2) x__(end:-1:3)]; 
+xtemp= [x(3:end) zeros(1,2)];  %x(2-n)shifthing right by 2unit then reflect
+x23= [ zeros(1,2) xtemp(end:-1:3)]; 
 
-y2=x21.*x22+x23.*x;
-subplot(3,1,2)
+y2=(x21.*x22)+(x23.*x);
+figure;
 stem(n,y2);
-
+grid on;
+figure;
 y3=n.*[zeros(1,1) x(1:end-1)] + n.*[zeros(1,2) x(1:end-2)]+ n.*[zeros(1,3) x(1:end-3)] + n.*[zeros(1,4) x(1:end-4)] +n.*[zeros(1,5) x(1:end-5)];
-subplot(3,1,3)
 stem(n,y3);
-
+grid on;
