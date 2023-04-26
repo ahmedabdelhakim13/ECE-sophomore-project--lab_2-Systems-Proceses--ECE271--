c1f80456d5e4c85478bba6b532d4990bee8b6d0f
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-26 10:46:31 +0200

    vfvf

--- task5.m
+++ task5.m
@@ -1,7 +1,31 @@
 clc
 
-x=[0,0,0,1,-2,4,6,-5,8,10,0,0,0];  %define the sequence
-n=[0,0,0,-4,-3,-2,-1,0,1,2,0,0,0];
+x=[1,-2,4,6,-5,8,10];  %define the sequence
+n=-4:2;
+subplot(4,2,1);
 stem(n,x);
+
+x11=3*x;
+n11=n-2;
+subplot(4,2,2);
+stem(n11,x11);
+
+x12=x;
+n12=n+4;
+subplot(4,2,3);
+stem(n12,x12);
+
+x13=2*x;
+n13=n;
+subplot(4,2,4);
+stem(n13,x13);
+
+for n=-10:10 ;
+    y1=x11+x12+x13;
+   
+end
+m=linspace(-6,6,7);
+subplot(4,2,[5: 8]);
+stem(m,y1);
 
 
