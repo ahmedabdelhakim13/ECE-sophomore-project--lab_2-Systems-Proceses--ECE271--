d6080235d26b8f34b5fb0a607ab17e13b8c3dc78
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-27 21:43:38 +0200

    بب

--- task4.m
+++ task4.m
@@ -1,44 +1,66 @@
 clear
-n=linspace(0,40,20);
+n=linspace(-20,20,20);
+%definng the period
 x=cos(2*n./3);
 y=cos(8*pi*n./38);
-subplot(3,2,1);
+subplot(2,2,1);
 stem(n,x,'k-o');
 xlabel('time');
 ylabel('frequency');
 grid on
 
-subplot(3,2,3);
+subplot(2,2,3);
 stem(n,y,'b-o');
 xlabel('time');
 ylabel('frequency');
 grid on
+%%changing the period of the signalto the double
+m=linspace(-40,40,40);
+w= resample(x,2,1);
+z= upsample(y,2,1);
 
-subplot(3,2,2);
-plot(n,x,'k-o');
+subplot(2,2,2);
+stem(m,w,'k-o');
 xlabel('time');
 ylabel('frequency');
 grid on
 
-subplot(3,2,4);
-plot(n,y,'b-o');
+subplot(2,2,4);
+stem(m,z,'k-o');
 xlabel('time');
 ylabel('frequency');
 grid on
 
-figure,stem(n,x,'r-o');
-hold on;
-stem(n,y,'b-x');
-grid on
+%%
+%%changing the frequency and period 
 
-m=xcorr(x,x)
-t1=n;
-t2=-fliplr(n)
-a1=min(t1)+min(t2);
-a2=max(t1)+max(t2);
-a=a1:1:a2;
-subplot(3,2,[5 6]);
+n=linspace(0,40,20)%because the sampling time T=2 so sampling freq is 0.5
+t=linspace(0,40,4000)%because it was said in the section that in cont time signals we assume the sampling rate to be 100
 
-size (t1);
-size (t2);
+x_of_n=cos((2.*n)./3)
+x_of_t=cos((2.*t)./3)
+
+y_of_n=cos((8.*pi.*n)./38)
+y_of_t=cos((8.*pi.*t)./38)
+
+subplot(2,2,1)
+stem(n,x_of_n)
+xlabel('n')
+ylabel('x(n)')
+
+subplot(2,2,2)
+plot(t,x_of_t)
+xlabel('t')
+ylabel('x(t)')
+
+subplot(2,2,3)
+stem(n,y_of_n)
+xlabel('n')
+ylabel('y(n)')
+
+subplot(2,2,4)
+plot(t,y_of_t)
+xlabel('t')
+ylabel('y(t)')
+
 
--- task8.m
+++ task8.m
@@ -1,17 +1,30 @@
-n=-10:10;
-real_part= exp(-0.1).*n;
-imaginary_part=exp(0.3*i).*n;
-signal= real_part .* imaginary_part;
+n=-10:1:10;
+signal=exp(n.*(-0.1+0.3i));
+Real_part=real(signal);  
+%function real returns the real part of the signal
+Imaginary_part=imag(signal);
+%function imaginary returns the imaginary part of the signal
+Magnitude=abs(signal);   
+%function abs returns the magnitude of the signal
+Angle=angle(signal);
+%function anglereturns the angle of the signal
 
-subplot(2,2,1);
-plot(n,abs(signal));
-
-subplot(2,2,2);
-plot(n,angle(signal));
-
-subplot(2,2,3);
-plot(n,real_part);
-
-
-subplot(2,2,4);
-plot(imaginary_part);
+subplot(2,2,1)
+stem(n,Real_part);
+grid on;
+ylabel('Real');
+%------------------;
+subplot(2,2,2)
+stem(n,Imaginary_part);
+grid on;
+ylabel('Imaginary');
+%------------------;
+subplot(2,2,3)
+stem(n,Magnitude);
+grid on;
+ylabel('Magnitude');
+%-------------------;
+subplot(2,2,4)
+stem(n,Angle);
+grid on;
+ylabel('Angle');
