357cc771b7488eeaddcf9beac0a779d9b1108b59
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-27 22:38:34 +0200

    Update task4.m

--- task4.m
+++ task4.m
@@ -5,13 +5,13 @@ x=cos(2*n./3);
 y=cos(8*pi*n./38);
 subplot(2,2,1);
 stem(n,x,'k-o');
-xlabel('time');
+xlabel('sampels');
 ylabel('frequency');
 grid on
 
 subplot(2,2,3);
 stem(n,y,'b-o');
-xlabel('time');
+xlabel('sampels');
 ylabel('frequency');
 grid on
 %%changing the period of the signalto the double
@@ -21,13 +21,13 @@ z= upsample(y,2,1);
 
 subplot(2,2,2);
 stem(m,w,'k-o');
-xlabel('time');
+xlabel('sampels');
 ylabel('frequency');
 grid on
 
 subplot(2,2,4);
 stem(m,z,'k-o');
-xlabel('time');
+xlabel('sampels');
 ylabel('frequency');
 grid on
 %for better vesion of the functions
@@ -42,33 +42,47 @@ stem(m,z,'k-o');
 
 %%changing the frequency and period 
 
-n=linspace(0,40,20)%because the sampling time T=2 so sampling freq is 0.5
-t=linspace(0,40,4000)%because it was said in the section that in cont time signals we assume the sampling rate to be 100
+n_new=linspace(0,40,20);
+t=linspace(0,40,4000);
 
-x_of_n=cos((2.*n)./3)
-x_of_t=cos((2.*t)./3)
+Xdis=cos((2.*n)./3);%disrete signal
+Xcont=cos((2.*t)./3);%contineous signal
 
-y_of_n=cos((8.*pi.*n)./38)
-y_of_t=cos((8.*pi.*t)./38)
+Ydis=cos((8.*pi.*n)./38)%disrete signal
+Ycont=cos((8.*pi.*t)./38)%contineous signal
 
 subplot(2,2,1)
-stem(n,x_of_n)
-xlabel('n')
-ylabel('x(n)')
+stem(n_new,Xdis)
+xlabel('sampels');
+ylabel('frequency');
+grid on;
 
-subplot(2,2,2)
-plot(t,x_of_t)
-xlabel('t')
-ylabel('x(t)')
+subplot(2,2,2);
+plot(t,Xcont);
+xlabel('time');
+ylabel('frequency');
+grid on;
 
-subplot(2,2,3)
-stem(n,y_of_n)
-xlabel('n')
-ylabel('y(n)')
+subplot(2,2,3);
+stem(n,Ydis);
+xlabel('sampels');
+ylabel('frequency');
+grid on;
 
-subplot(2,2,4)
-plot(t,y_of_t)
-xlabel('t')
-ylabel('y(t)')
+subplot(2,2,4);
+plot(t,Ycont);
+xlabel('time');
+ylabel('frequency)');
+grid on;
+
+%for better vesion of the functions
+figure;
+stem(n_new,Xdis,'k-o');
+figure;
+stem(n_new,Ydis,'k-o');
+figure;
+plot(t,Xcont);
+figure;
+plot(t,Ycont);
 
 
